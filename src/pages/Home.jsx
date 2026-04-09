86e7068be90d3243a8f9703612c12cff1f5e2839
Veeraj <veerajrao05@gmail.com>
2026-04-09 22:40:55 +0530

    added time and greetings

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react"
 import BottomNav from "../components/shared/BottomNav"
+import Greetings from  "../components/home/Greetings"
 const Home =() =>{
     return(
         <section className="bg-[#1f1f1f] h-[calc(100vh-7rem)] overflow-hidden flex gap-3">
         {/*Left-Div*/}
-        <div className="flex-[3] bg-red-600"></div>
+        <div className="flex-[3]">
+            <Greetings/>
+        </div>
         {/*Right-Div*/}
         <div className="flex-[2] bg-blue-600"></div>
         <BottomNav/>
